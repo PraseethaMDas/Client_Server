3a1ade039e58efddda14f216a67f516350b5db59
praseetha <praseethamdas198@gmail.com>
2018-03-24 11:56:51 +0530

    messaging added

--- Server.java
+++ Server.java
@@ -9,6 +9,13 @@ class Server
     	DataInputStream din=new DataInputStream(s.getInputStream());  
     	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     	BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
-    	String str="",str2="";   
+    	String str="",str2="";  
+    	while(!str.equals("stop")){  
+		str=din.readUTF();  
+		System.out.println("client says: "+str);  
+		str2=br.readLine();  
+		dout.writeUTF(str2);  
+		dout.flush();  
+		} 
 	}
 }
