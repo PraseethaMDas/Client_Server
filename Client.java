dd23cd037445b01a48bd38ca8797d43eddc3a68d
nisha <nishavava1996@gmail.com>
2018-03-24 12:05:17 +0530

    String variable initialized and perform

--- Client.java
+++ Client.java
@@ -6,4 +6,8 @@ import java.io.*;
     DataInputStream din=new DataInputStream(s.getInputStream());  
     DataOutputStream dout=new DataOutputStream(s.getOutputStream());
     BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-      
+    String str="",str2="";  
+    while(!str.equals("stop")){  
+    str=br.readLine();  
+    dout.writeUTF(str);  
+    dout.flush();   
