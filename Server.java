0bf0267bd9552cc1d591990c646b5d043bf6e344
praseetha <praseethamdas198@gmail.com>
2018-03-24 12:01:42 +0530

    exception thrown

--- Server.java
+++ Server.java
@@ -2,7 +2,7 @@ import java.net.*;
 import java.io.*; 
 class Server
 {
-	public static void main(String args[])
+	public static void main(String args[])throws Exception
 	{
 		ServerSocket ss=new ServerSocket(3333);  
     	Socket s=ss.accept();  
@@ -10,7 +10,8 @@ class Server
     	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
     	BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
     	String str="",str2="";  
-    	while(!str.equals("stop")){  
+    	while(!str.equals("stop"))
+    	{  
 			str=din.readUTF();  
 			System.out.println("client says: "+str);  
 			str2=br.readLine();  
