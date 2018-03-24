7188d9c9dcabc4714aea460db4eec9c015b21707
PraseethaMDas <36188505+PraseethaMDas@users.noreply.github.com>
2018-03-24 12:05:14 +0530

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,21 +5,21 @@ class Server
 	public static void main(String args[])throws Exception
 	{
 		ServerSocket ss=new ServerSocket(3333);  
-    	Socket s=ss.accept();  
-    	DataInputStream din=new DataInputStream(s.getInputStream());  
-    	DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-    	BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
-    	String str="",str2="";  
-    	while(!str.equals("stop"))
-    	{  
+    		Socket s=ss.accept();  
+    		DataInputStream din=new DataInputStream(s.getInputStream());  
+    		DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+    		BufferedReader br=new BufferedReader(new InputStreamReader(System.in)); 
+    		String str="",str2="";  
+    		while(!str.equals("stop"))
+    		{  
 			str=din.readUTF();  
 			System.out.println("client says: "+str);  
 			str2=br.readLine();  
 			dout.writeUTF(str2);  
 			dout.flush();  
-	} 
-	din.close();  
-    	s.close();  
-    	ss.close();  
+		} 
+		din.close();  
+		s.close();  
+		ss.close();  
 	}
 }
